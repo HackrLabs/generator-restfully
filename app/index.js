11ac044de5e0fe5de6643dd3ba183201d82ceb6b
Danny Grove <danny@drgrovellc.com>
2014-02-22 12:41:34 -0500

    Adds prompt for database module to be installed

--- app/index.js
+++ app/index.js
@@ -23,28 +23,30 @@ var RestfullyGenerator = yeoman.generators.Base.extend({
     console.log(this.yeoman);
 
     // replace it with a short and sweet description of your generator
-    console.log(chalk.magenta('You\'re using the fantastic Restfully generator.'));
+    console.log(chalk.magenta('Welcome to Restfully. A RESTFUL API Generator!'));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    var prompts = [
+    { type: 'list'
+    , name: 'database'
+    , message: 'Which database will you be using?'
+    , choices: ['MySQL', 'PostgeSQL', 'Sqlite3']
+    }
+    ]
 
     this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
+      this.database = props.database;
 
       done();
     }.bind(this));
   },
 
   app: function () {
-    this.mkdir('app');
-    this.mkdir('app/templates');
+    this.mkdir('lib');
 
     this.copy('_package.json', 'package.json');
-    this.copy('_bower.json', 'bower.json');
+    this.npmInstall( 'bookshelf', {save: true} );
+    // Install Added Dependencies
+    this.npmInstall( getNPMdb(this.database), {save: true} );
   },
 
   projectfiles: function () {
@@ -52,5 +54,28 @@ var RestfullyGenerator = yeoman.generators.Base.extend({
     this.copy('jshintrc', '.jshintrc');
   }
 });
+
+/**
+ * Get the NPM Package Name for the Database
+ * @private
+ * @param {string} databae
+ * @returns {string} theDB
+ */
+function getNPMdb( database ){
+  var theDB = '';
+  switch( database ){
+    case 'MySQL':
+      theDB = 'mysql';
+      break;
+    case 'PostgreSQL':
+      theDB = 'pg';
+      break;
+    case 'Sqlite3':
+      theDB = 'sqlite3';
+      break;
+  }
+
+  return theDB;
+}
 
 module.exports = RestfullyGenerator;
